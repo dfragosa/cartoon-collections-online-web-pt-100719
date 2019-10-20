65968d016b5a41a9f6a42d35cc632191fec65d8f
Derric Fragosa <fderric@gmail.com>
2019-10-20 20:15:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(words)
    
 end 
    
-def find_the_cheese() # code an argument here
+def find_the_cheese(str_array) 
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
