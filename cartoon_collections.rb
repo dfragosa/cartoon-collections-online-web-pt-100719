e4415ad1e9404e78a480af7d26c371f3dc56efcb
Derric Fragosa <fderric@gmail.com>
2019-10-20 20:45:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(words)
 end 
    
 def find_the_cheese(str_array) 
-   str_array.include?("cheddar", "gouda", "camembert")
+   
      # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.all? {|i| (0..-1).include?(i)}
